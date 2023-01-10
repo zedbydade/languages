fedff7f1271950c34742e01fd355e524ec1c0860
Francisco Paradela <franciscoleite.dev@protonmail.com>
2023-01-10 20:19:31 -0300

    First commit

---
+++ ruby/sinatra_pub_sub/app/controllers/application_controller.rb
@@ -0,0 +1,5 @@
+class ApplicationController < Sinatra::Base
+  get '/' do
+    'Welcome to the goat2'
+  end
+end
---
+++ ruby/sinatra_pub_sub/app/controllers/public_controller.rb
@@ -0,0 +1,5 @@
+class PublicController < Sinatra::Base
+  get '/' do
+    'Welcome to the goat2'
+  end
+end
