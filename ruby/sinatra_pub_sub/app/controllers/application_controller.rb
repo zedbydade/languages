ca66218f17521f3eb437fd70e7dd96c55e367ac4
Francisco Paradela <franciscoleite.dev@protonmail.com>
2023-01-10 20:34:58 -0300

    change require_all for a more specific strategy to load files

--- ruby/sinatra_pub_sub/app/controllers/application_controller.rb
+++ ruby/sinatra_pub_sub/app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < Sinatra::Base
-  get '/' do
-    'Welcome to the goat2'
-  end
 end
--- ruby/sinatra_pub_sub/config/environment.rb
+++ ruby/sinatra_pub_sub/config/environment.rb
@@ -1,4 +1,7 @@
 require 'bundler'
 Bundler.require
 
-require_all 'app'
+%w[models routes].each do |dir|
+  $LOAD_PATH << File.expand_path('.', File.join(File.dirname(__FILE__), dirr))
+  require File.join(dir, 'init')
+end
