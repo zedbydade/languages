bc48559c527855881ee21c322f4dad02e71012a6
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-02-28 00:23:46 -0300

    Add chapter_2

---
+++ elixir/learning/chapter_2/init.ex
@@ -0,0 +1,19 @@
+# 5.0
+10 / 2 
+
+# 5
+div 10, 2
+
+is_boolean(true)
+
+is_boolean(1)
+
+string = :world
+
+"Hello #{string}"
+
+add = fn a, b  -> a + b end
+
+double = fn a -> add.(a, a) end
+
+double.(2)
