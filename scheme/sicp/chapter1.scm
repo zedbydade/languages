ed40e3231eb944862bb6c4aa731cf1ff6b4432f2
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-02-28 22:35:29 -0300

    Add new_chapter1 code

--- scheme/sicp/chapter1.scm
+++ scheme/sicp/chapter1.scm
@@ -18,3 +18,15 @@
 (define size 2)
 (* 5 size)
 
+(* (+ 2 (* 4 6))
+   (+ 3 5 6))
+
+(define (square x) (* x x))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(define (abs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) (x -))))
