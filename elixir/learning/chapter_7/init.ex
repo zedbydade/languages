fc61a72cb0182d8ad5d5c4a86118df6e2f2139fa
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-03-15 22:37:29 -0300

    Add chapter_

---
+++ elixir/learning/chapter_7/init.ex
@@ -0,0 +1,8 @@
+String.split('1, 2, 3', ' ', [trim: true])
+String.split('1, 2, 3', ' ', trim: true)
+
+list = [a: 1, b: 2]
+
+map = %{:a => 1, 2=> b}
+
+if true, do: "This will be seen", else: "This won't"
--- scheme/sicp/chapter1.scm
+++ scheme/sicp/chapter1.scm
@@ -30,3 +30,18 @@
   (cond ((> x 0) x)
         ((= x 0) 0)
         ((< x 0) (x -))))
+
+(define (sqrt-iter guexx x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+(define (improve guess x
+                 (average guess (/ x guess))))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
