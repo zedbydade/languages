440788ba482dd9458068c4a1560ceb50f2a568ca
francisco paradela <franciscoleite.dev@protonmail.com>
2023-08-12 13:12:47 -0300

    Add inspect implementation

--- elixir/portal/lib/portal.ex
+++ elixir/portal/lib/portal.ex
@@ -18,3 +18,23 @@ defmodule Portal do
     portal
   end
 end
+
+
+defimpl Inspect, for: Portal do
+  def inspect(%Portal{left: left, right: right}, _) do
+    left_door  = inspect(left)
+    right_door = inspect(right)
+
+    left_data  = inspect(Enum.reverse(Portal.Door.get(left)))
+    right_data = inspect(Portal.Door.get(right))
+
+    max = max(String.length(left_door), String.length(left_data))
+
+    """
+    #Portal<
+      #{String.pad_leading(left_door, max)} <=> #{right_door}
+      #{String.pad_leading(left_data, max)} <=> #{right_data}
+    >
+    """
+  end
+end
