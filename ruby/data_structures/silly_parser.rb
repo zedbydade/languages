b6882959c39dc03a1f1f300b7b7d36083c2f7f73
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-08-29 19:23:32 -0300

    add silly_parser

---
+++ ruby/data_structures/silly_parser.rb
@@ -0,0 +1,15 @@
+begin 
+  raise "lmao"
+rescue => 
+  (class Aria 
+    class << self 
+      attr_accessor :tate
+    end
+  end; Aria).tate
+end
+
+h = {}
+begin 
+  raise "omg"
+rescue => h[:the_key]
+end
