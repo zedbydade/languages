6a37d70a2dfd2be5662f3767472ca00745233fad
Francisco Paradella <franciscoleite.dev@protonmail.com>
2024-02-07 20:03:23 -0300

    Add emacs_lisp

---
+++ emacs_lisp/syntax.el
@@ -0,0 +1,23 @@
+(defun the-meaning-of-life (answer)
+  (message "The answer is %s" answer))
+
+(list 1 2 3
+      4 5 6
+      7 8 9)
+
+42
+
+(* 42 10)
+
+(concat "TU-TU" "-RU")
+
+(car '(1 2 3))
+
+[1 2 3]
+
+(defun add-node-to-tail(head value)
+  (defvar temp '(value, nil))
+  (message "%s" temp)
+)
+
+(add-node-to-tail (list) 20)
