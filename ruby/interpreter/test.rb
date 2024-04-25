981ae2a506c7a208083e177ee78ada6920aabc5b
Francisco Leite <franciscoleite.dev@protonmail.com>
2024-04-25 19:43:27 -0300

    Add conditions and variable assign

--- ruby/interpreter/test.rb
+++ ruby/interpreter/test.rb
@@ -17,4 +17,17 @@ class TestUbyInterpreter < Minitest::Test
     assert_eval(3, '3')
     assert_eval(7, '3 + 4')
   end
+
+  def test_lvar
+    assert_eval 42, "x = 42; x"
+  end
+
+  def test_if
+    assert_eval 42, 'if true  then 42 else 24 end'
+  end
+
+  def test_if_falsey 
+    assert_eval 24, "if nil  then 42 else 24 end"
+    assert_eval 24, "if false then 42 else 24 end"
+  end
 end
--- ruby/interpreter/uby_interpreter.rb
+++ ruby/interpreter/uby_interpreter.rb
@@ -4,12 +4,56 @@ require 'ruby_parser'
 class UbyInterpreter < SexpInterpreter
   VERSION = '1.0.0'
 
-  attr_accessor :parser
+  attr_accessor :parser, :env
 
   def initialize
     super
 
     self.parser = RubyParser.new
+    self.env = {}
+  end
+
+  def process_lvar(s)
+    _, name = s
+
+    env[name]
+  end
+
+  def process_lasgn(s)
+    _, n, v = s
+    env[n] = process(v)
+  end
+
+  def process_block(s)
+    result = nil
+    s.rest.each do |sub|
+      result = process(sub)
+    end
+    result
+  end
+
+  def process_nil(_s)
+    nil
+  end
+
+  def process_true(_s)
+    true
+  end
+
+  def process_false(_s)
+    false
+  end
+
+  def process_if(s)
+    _, c, t, f = s
+
+    c = process(c)
+
+    if c
+      process t
+    else
+      process f
+    end
   end
 
   def process_call(s)
