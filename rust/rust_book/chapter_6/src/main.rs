0d2b6a0e47b5346542ad958bfab7b784539446ac
Francisco Leite <franciscoleite.dev@protonmail.com>
2024-08-05 22:17:04 -0300

    chapter_6

---
+++ rust/rust_book/chapter_6/src/main.rs
@@ -0,0 +1,64 @@
+fn main() {
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    //  enum IpAddr {
+    //      V4(String),
+    //      V6(String),
+    // }
+    //
+
+    #[derive(Debug)]
+    enum UsState {
+        Alabama,
+        Alaska,
+    }
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState),
+    }
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("State quarter from {state:?}!");
+                25
+            }
+        }
+    }
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let config_max = Some(3u8); 
+    if let Some(max) = config_max { 
+        println!("{max}")
+    }
+}
