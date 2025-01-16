3baf88117bd280bf679ef7bd5330445347f0beb7
Francisc Leite <franciscoleite.dev@protonmail.com>
2025-01-15 21:00:44 -0300

    Add test_switch_non_exhaustive

--- zig/hello_world/src/main.zig
+++ zig/hello_world/src/main.zig
@@ -3,9 +3,6 @@ const std = @import("std");
 pub fn main() !void {
     std.debug.print(("TU-TU-RU"), .{});
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
